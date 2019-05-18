3425579e6081f349391303917cde8676381f680a
equation <sivasaiemail@gmail.com>
2019-05-18 15:46:38 -0400

    gitignore

---
+++ ListComprehension.py
@@ -0,0 +1,29 @@
+"""
+Demo of different types List comprehension in python
+"""
+
+
+"""Below is basic list comprehension
+"""
+numbers = [1,2,3,4,5,6,7,8,9,10]
+print(numbers)
+
+square = [ n*n for n in numbers]
+print(square)
+
+
+"""Conditionaly pick elements in list
+"""
+
+def minus1(nm):
+    return nm-1
+
+evenMinus1 = [ minus1(n) for n in numbers if n%2 ==0]
+print(evenMinus1)
+
+
+
+
+
+
+
